5ced5614f1595863cc5c36604ea417443605f387
meghark <98137756+meghark@users.noreply.github.com>
2022-04-28 11:32:33 -0700

    Added missing fields in select statements

--- lib/Role.cs
+++ lib/Role.cs
@@ -4,9 +4,12 @@ const TableQuery = require('./TableQuery);
  Class Role extends TableQuery
  {
      constructor(){
-         this.select = `SELECT * FROM role;`;
+         this.commonSelect =  `SELECT rl.title as 'Job Title', rl.id,dep.name as department ,rl.salary  
+                                FROM role rl 
+                                left join department dep on dep.id = rl.department_id`;
+         this.select = `${this.commonSelect};`;
          this.insert = `INSERT INTO role(title, salary, department_id) VALUES (?,?,?);`;
-         this.selectById = `SELECT * FROM role WHERE id = ?`;
+         this.selectById = `${this.commonSelect} WHERE id = ?`;
          this.delete =  `DELETE FROM role WHERE id = ?`;
      }    
  }
